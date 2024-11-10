ecab9782bf7f7ca14b9ac8beef0e74a1b4a50970
Anitha4229 <168728181+Anitha4229@users.noreply.github.com>
2024-11-10 21:44:56 +0530

    Update2 login.jsx

--- Frontend/src/Components/login.jsx
+++ Frontend/src/Components/login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://msd-final-project-1.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
